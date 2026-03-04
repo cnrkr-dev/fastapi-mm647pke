874813b7d68bdffe5013bc100acdd660fb86fd4b
caner <cnrkr.dev@gmail.com>
2026-03-04 10:42:20 +0300

    Initialize Supabase client with environment variables

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ load_dotenv()
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
 
 @app.get("/")
 async def root():
